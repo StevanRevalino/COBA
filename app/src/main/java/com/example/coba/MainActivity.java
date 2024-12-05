3653548aa1ef398ed56fb7f09e10f39b8996ca55
YamiZen24 <YamiZen24>
2024-12-05 21:20:28 +0700

    ganti var dari cibai -> p

--- app/src/main/java/com/example/coba/MainActivity.java
+++ app/src/main/java/com/example/coba/MainActivity.java
@@ -10,7 +10,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    String cibai;
+    String p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +23,6 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        cibai = new String();
+        p = new String();
     }
 }
